bb4264b81d24ba2c41aa4dd49d19ddc810c1f4c4
Saraneshkanna Vivek <101494107+Saraneshkanna@users.noreply.github.com>
2023-07-08 18:42:25 +0530

    Experience Page Completed!

--- myportfolio/src/pages/Experience.js
+++ myportfolio/src/pages/Experience.js
@@ -8,6 +8,16 @@ import Navbar from '../components/Navbar';
 import ReactPic from "../images/tech/React.png"
 import FigmaPic from "../images/tech/figma.png"
 import intellectPic from "../images/Timeline/intellect.jpeg"
+import vifrPic from "../images/Timeline/vifr.jpeg"
+import unityPic from "../images/tech/unity.png"
+import gitPic from "../images/tech/git.png"
+import blenderPic from "../images/tech/Blender.jpg"
+import CSpic from "../images/tech/CSharp.png"
+import VITPic from "../images/Timeline/VIT.png"
+import hsvPic from "../images/Timeline/hsv.png"
+import jsPic from "../images/tech/JS.png"
+import HTMLPic from "../images/tech/HTML.png"
+import CSSPic from "../images/tech/CSS.png"
 import '../styles/Experience.css'
 
 function Experience() {
@@ -17,11 +27,15 @@ function Experience() {
             <Navbar />
         </div>
         <div className='timeline'>
-          <VerticalTimeline lineColor='#3e497a'>
+          <div>
+            <h1>My Education & Experience:</h1>
+          </div>
+          <VerticalTimeline lineColor='#ffffff'>
               <VerticalTimelineElement 
                   className='vertical-timeline-element--intellect'
-                  date="June 2023"
-                  iconStyle={{background : "#3e497a", color:"#fff"}}
+                  date="JUNE 2023"
+                  dateClassName='date'
+                  iconStyle={{background : "#e28743", color:"#fff"}}
                   icon={<WorkIcon />}
                   >
                     <div className='work-desc'>
@@ -37,47 +51,94 @@ function Experience() {
                       <p>Enter workflow description here orem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit sapien non ex pharetra consectetur. Vestibulum non gravida lectus. Proin commodo malesuada enim, ut convallis eros viverra sit amet. Ut consectetur lacinia nulla in rutrum. Maecenas convallis mi nec orci scelerisque, ac scelerisque lectus ultrices. Curabitur et dolor nec nunc sagittis finibus</p>
                       <div className='toolIcon'>
 
-                        <p className='tools'><ConstructionIcon /> I used:</p>
+                        <p className='tools'><ConstructionIcon /> used:</p>
                       </div>
-                      
-                      <img src={ReactPic}></img>
                       <img src={FigmaPic}></img>
+                      <img src={HTMLPic}></img>
+                      <img src={CSSPic}></img>
+                      <img src={jsPic}></img>
+                      <img src={ReactPic}></img>
+
               </VerticalTimelineElement> 
               <VerticalTimelineElement 
                   className='vertical-timeline-element--vifr'
                   date="May 2022 - July 2022"
-                  iconStyle={{background : "#3e497a", color:"#fff"}}
+                  iconStyle={{background : "#e28743", color:"#fff"}}
                   icon={<WorkIcon />}
                   >
-                      <h2 className='vertical-timeline-element-title'>Unity Developer Intern</h2>
-                      <p>VIFR Labs (Remote)</p>
+                      <div className='work-desc'>
+                      <div>
+                        <img className='vifrLogo' src={vifrPic} />
+                      </div>
+                      <div className='vertical-timeline-element-title'>
+                        <h2 >Unity3D Intern </h2>
+                        <h3>VIFR Labs (Remote)</h3>
+                      </div>
+                      
+                    </div>
+                    
+                      <p>Enter workflow description here orem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit sapien non ex pharetra consectetur. Vestibulum non gravida lectus. Proin commodo malesuada enim, ut convallis eros viverra sit amet. Ut consectetur lacinia nulla in rutrum. Maecenas convallis mi nec orci scelerisque, ac scelerisque lectus ultrices. Curabitur et dolor nec nunc sagittis finibus</p>
+                      <div className='toolIcon'>
+
+                        <p className='tools'><ConstructionIcon /> used:</p>
+                      </div>
+                      <img src={unityPic}></img>
+                      <img src={CSpic}></img>
+                      <img src={gitPic}></img>
+                      <img src={blenderPic}></img>
               </VerticalTimelineElement> 
               <VerticalTimelineElement 
                   className='vertical-timeline-element--education'
                   date="2020-2024"
-                  iconStyle={{background : "#3e497a", color:"#fff"}}
+                  iconStyle={{background : "#cc1020", color:"#fff"}}
                   icon={<SchoolIcon />}
                   >
-                      <h2 className='vertical-timeline-element-title'>B.Tech Computer Science Engineering</h2>
-                      <p>VIT University, Vellore</p>
+                       <div className='work-desc'>
+                      <div className='vertical-timeline-element-title'>
+                        <h2 >B.Tech Computer Science Engineering  </h2>
+                        <h3>VIT University, Vellore</h3>
+                        <p>CGPA: 8.99</p>
+                      </div>
+                      <div>
+                        <img src={VITPic} />
+                      </div>
+                      
+                    </div>
               </VerticalTimelineElement> 
               <VerticalTimelineElement 
                   className='vertical-timeline-element--education'
-                  date="2020-2024"
-                  iconStyle={{background : "#3e497a", color:"#fff"}}
+                  date="2020"
+                  iconStyle={{background : "#cc1020", color:"#fff"}}
                   icon={<SchoolIcon />}
                   >
-                      <h2 className='vertical-timeline-element-title'>Grade XII</h2>
-                      <p>Chettinad Harishree Vidyalayam, Chennai</p>
+                       <div className='work-desc'>
+                      <div>
+                        <img src={hsvPic} />
+                      </div>
+                      <div className='vertical-timeline-element-title'>
+                        <h2 >Grade XII </h2>
+                        <h3>Chettinad Harishree Vidyalayam</h3>
+                        <p>Grade: 96.2% (ISC)</p>
+                      </div>
+                    </div>
               </VerticalTimelineElement> 
               <VerticalTimelineElement 
                   className='vertical-timeline-element--education'
-                  date="2020-2024"
-                  iconStyle={{background : "#3e497a", color:"#fff"}}
+                  date="2018"
+                  iconStyle={{background : "#cc1020", color:"#fff"}}
                   icon={<SchoolIcon />}
                   >
-                      <h2 className='vertical-timeline-element-title'>Grade X</h2>
-                      <p>Chettinad Harishree Vidyalayam, Chennai</p>
+                      <div className='work-desc'>
+                      <div className='vertical-timeline-element-title'>
+                        <h2 >Grade X </h2>
+                        <h3>Chettinad Harishree Vidyalayam</h3>
+                        <p>Grade: 95.6% (ICSE)</p>
+                      </div>
+                      <div>
+                        <img src={hsvPic} />
+                      </div>
+                      
+                    </div>
               </VerticalTimelineElement> 
           </VerticalTimeline>
         </div>
